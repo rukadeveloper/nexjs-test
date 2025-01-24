65121b631e5754b1b0a427e9bfce5d6d21177142
rukadeveloper <kansaki135@gmail.com>
2025-01-25 00:09:04 +0900

    basic setup, shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Course!</h1>
+      <Button>Click Here</Button>
     </div>
   );
 }
